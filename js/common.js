e0549f0dadce3b8321717ffe3c37ebf2dafc6437
kimiyais <kimiyais@users.noreply.github.com>
2025-04-01 01:38:35 +0000

    deploy: 3637e3efdf8aa743802b47d9eeb21ae181866b83

--- js/common.js
+++ js/common.js
@@ -1,22 +1,18 @@
 const menuHamburger = document.querySelector(".ki-burger");
-const tsNavbarContainer = document.querySelector(".ki-navbar-container");
-const tsNavbarLogo = document.querySelector(
-  ".ki-navbar--light .ki-navbar__logo img"
-);
-let isNavbarActive = tsNavbarContainer.classList.contains("ki-navbar-active");
+const kiNavbarContainer = document.querySelector(".ki-navbar-container");
+let isNavbarActive = kiNavbarContainer.classList.contains("ki-navbar-active");
 
 let navLinks = document.querySelectorAll(".ki-navbar__link");
 
 function toggleNavbar(onlyClose) {
   if (onlyClose) {
-    tsNavbarContainer.classList.remove("ki-navbar-active");
+    kiNavbarContainer.classList.remove("ki-navbar-active");
   } else {
-    tsNavbarContainer.classList.toggle("ki-navbar-active");
+    kiNavbarContainer.classList.toggle("ki-navbar-active");
   }
 }
 
 menuHamburger.addEventListener("click", () => toggleNavbar(false));
-
 
 let closeNavbar = () => {
   // Only close Navbar when clicked
